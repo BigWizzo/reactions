4ab8fef8840f498537ee8aca245485b999bce83a
bigwizzo <wnnwillard@gmail.com>
2021-04-09 18:35:25 +0200

    Iterate through the book and pass in object values on calling <Book />

--- src/index.js
+++ src/index.js
@@ -3,34 +3,42 @@ import ReactDom from "react-dom";
 
 // const author = "Will Nyamunokora";
 // const desc = "The best selling book";
-const myBook1 = {
-  img:
-    "https://m.media-amazon.com/images/I/51nRHsdkmFL._AC_UL480_FMwebp_QL65_.jpg",
-  author: "Joyce N",
-  desc: "Best Author",
-};
+const books = [
+  {
+    id: 1,
+    img:
+      "https://m.media-amazon.com/images/I/51nRHsdkmFL._AC_UL480_FMwebp_QL65_.jpg",
+    author: "Joyce N",
+    desc: "Best Author",
+  },
+  {
+    id: 2,
+    img:
+      "https://m.media-amazon.com/images/I/51nRHsdkmFL._AC_UL480_FMwebp_QL65_.jpg",
+    author: "Will N",
+    desc: "Best of Authors",
+  },
+];
 
-const myBook2 = {
-  img:
-    "https://m.media-amazon.com/images/I/51nRHsdkmFL._AC_UL480_FMwebp_QL65_.jpg",
-  author: "Will N",
-  desc: "Best of Authors",
-};
 function BookList() {
   return (
     <section>
-      <Book img={myBook1.img} author={myBook1.author} desc={myBook1.desc}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, velit
-          quia debitis vero aut itaque quam explicabo illo ad maxime.
-        </p>
-      </Book>
-      <Book img={myBook2.img} author={myBook2.author} desc={myBook2.desc} />
+      {books.map((book) => {
+        return (
+          <Book
+            key={book.id}
+            img={book.img}
+            author={book.author}
+            desc={book.desc}
+          />
+        );
+      })}
     </section>
   );
 }
 
-const Book = ({ img, author, desc, children }) => {
+const Book = (props) => {
+  const { img, author, desc, children } = props;
   return (
     <article>
       <img src={img} alt='' />
